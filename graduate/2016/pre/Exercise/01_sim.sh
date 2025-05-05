46da3b8635aa4214437c057ad0483e96e3f78ab9
noyaboy <science103555@gmail.com>
2025-05-05 23:43:06 +0800

    fix 01 sim

--- graduate/2016/pre/Exercise/01_sim.sh
+++ graduate/2016/pre/Exercise/01_sim.sh
@@ -1,16 +1,1 @@
-mkdir -p log/
-# 1. Compile Stage
-vcs -full64 -sverilog \
-    +libext+.v \
-    +timescale=1ns/1ps \
-    testfixture.v LBP.v \
-    -debug_all \
-    -l log/compile.log \
-    -o log/simv
-
-# 2. Simulation Execution
-./log/simv \
-    +vcs+lic+wait \
-    +ntb_random_seed=1234 \
-    +vcs+finish+1000 \
-    -l log/sim.log
+vcs testfixture.v LBP.v -R -full64 -debug_access+all +v2k
